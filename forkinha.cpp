cf013fab86ad444e300407b7bcaf041f8906cdf2
Thaisvc <thaisviana081@outlook.com>
2021-09-14 23:28:28 -0300

    forca2

---
+++ forkinha.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+
+using namespace  std ;
+
+int  main () {
+
+    char palavra [ 30 ] ;
+    
+    cout << "escreva a palavra :" << endl ;
+    cin >> palavra ;
+   
+   
+	}
+    
+
